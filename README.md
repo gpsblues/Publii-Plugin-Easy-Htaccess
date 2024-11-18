802bbe1ad69aafd209050b19b37371a913fe547f
gpsblues <vittorio.ramponi@gmail.com>
2024-11-18 10:56:59 +0100

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Publii Plugin: Easy .htaccess
 An easy .htaccess editor for Publii.
+
+<p><img height="100" alt="publii plugin" title="Copy text icons" src="https://raw.githubusercontent.com/gpsblues/Publii-Plugin-Easy-Htaccess/refs/head/main/.assets/thumbnail.svg"></p>
+
+## Features
+This plugin creates an .htaccess file with essential optimizations to enhance site performance. It includes HTTPS and 404 redirections, Gzip file compression, and custom directives. Please note that modifying the .htaccess file is at your own risk; the author assumes no responsibility for malfunctions or data loss. **A backup of the original .htaccess file is strongly recommended**, as these changes cannot be undone. This feature only works on Apache servers with .htaccess support.
+
+This plugin uses directives aimed at maximizing compatibility across different servers. However, compatibility is not guaranteed as the behavior of these directives can vary significantly depending on server configurations, which may differ widely. It's recommended to review and test the generated .htaccess file to ensure it functions as expected on your specific server environment.
+
+## Installation and Usage
+- Download the .zip file of the latest plugin version from the [release page](https://github.com/gpsblues/Publii-Plugin-Easy-Htaccess/releases/).
+- Open Publii CMS and [install the plugin](https://getpublii.com/docs/plugins.html#installingplugins).
+- [Enable the plugin](https://getpublii.com/docs/plugins.html#enablingplugins).
+- Set plugin options by clicking on its name.
+- Save and or Sync your website to see it in action.
+
+## Settings
+The plugin allows for selecting multiple options. These options will only take effect after synchronizing the site to an Apache server.
+
+**File Name**  
+- `htaccess.txt` creates a plain text file on the server, which will remain inactive until manually renamed to `.htaccess`. This is a safe option that allows you to review the file before making it operational.  
+- `.htaccess` directly creates the file, overwriting any previous versions.
+
+**Rewrite URL**  
+Enable HTTPS/HTTP redirection and enforce the use of the www or non-www prefix. Various rewrite combinations are available for flexibility.
+
+**Redirect 404**  
+Force 404 redirects to the `404.html` page. This works only if the theme supports error pages. [Learn more about supported features](https://getpublii.com/dev/theme-supported-features/).
+
+**Gzip Compression**  
+Enable Gzip compression to reduce file sizes and improve loading times. This feature requires `mod_deflate` to be enabled on the server.
+
+**Custom Directives**  
+Manually add your custom directives here. These will be appended to the end of the `.htaccess` file.
+
+**Empty .htaccess**  
+Reset all customizations by creating an empty `.htaccess` file. This option is effectively equivalent to deleting the file.
+
+
+## Uninstalling
+Since Publii plugins do not work directly on the server and currently cannot delete files, uninstalling or deactivating the plugin will leave the last version of the .htaccess file created by the plugin on the server. If you wish to remove this file, you have two options:  
+1. Manually delete the file directly from the server (the obvious option);  
+2. Enable the plugin's "Empty .htaccess" option and sync the site.
+
+Option 2 does not actually delete the .htaccess file but empties it, removing all directives. This effectively renders the file inactive, making its impact equivalent to deletion.
+
+## Note
+If you want to enable the copy function for code snippets, I recommend using the official [Syntax Highlighter plugin](https://marketplace.getpublii.com/plugins/syntax-highlighter/).
+
+## Disclaimer
+This plugin is an unofficial extension for the [Publii CMS](https://getpublii.com/). I do not assume any responsibility for potential issues or malfunctions that may occur while using this plugin. Additionally, support for this plugin is not guaranteed.
+
+For official Publii resources, please visit the [Publii CMS Official Repository](https://marketplace.getpublii.com/plugins/).
---
+++ easy-htaccess/main.js
@@ -0,0 +1,181 @@
+// https://github.com/GetPublii/Publii/discussions/1359
+
+class MyPlugin {
+    constructor (API, name, config) {
+        this.API = API; 		// gives you an access to the plugins API functions
+		this.name = name; 		// retrieved plugin name - probably will be removed in the future
+		this.config = config; 	// gives access to the plugin saved config
+    }	
+
+	
+	// Events - can be used to do some additional actions in specific moments of the website rendering process.
+    addEvents () {
+		this.API.addEvent('beforeRender', this.addFile, 1, this);
+    }
+
+    // Custom code below
+    
+	
+    addFile (rendererInstance) {	
+
+		//Output file name
+		let output = this.config.output
+		let content = ""
+
+		//Create empty .htaccess
+		if (this.config.destroy) {
+			this.API.createFile(`[ROOT-FILES]/${output}`, content, this);
+			return
+		}
+
+		//Rewrite URL
+		let rewriteUrl = ''
+		if (this.config.rewriteUrl) {
+			rewriteUrl = 
+			`	RewriteEngine On
+
+			`
+			switch (this.config.redirect) {
+				case '1':
+					rewriteUrl += 
+					`	# Redirect everything to https + www
+						RewriteCond %{HTTPS} off [OR]
+						RewriteCond %{HTTP_HOST} !^www\. [NC]
+						RewriteRule ^ https://www.%{HTTP_HOST}/%{REQUEST_URI} [L,R=301]
+
+					`;
+					break;
+			
+				case '2':
+					rewriteUrl += 
+					`	# Redirect everything to https + no-www
+						RewriteCond %{HTTPS} off [OR]
+						RewriteCond %{HTTP_HOST} ^www\. [NC]
+						RewriteRule ^ https://%{HTTP_HOST}:%{SERVER_PORT}/%{REQUEST_URI} [L,R=301,NE]
+
+					`;
+					break;
+			
+				case '3':
+					rewriteUrl += 
+					`	# Redirect everything to https (no preference for www or no-www)
+						RewriteCond %{HTTPS} off
+						RewriteRule ^ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+
+					`;
+					break;
+			
+				case '4':
+					rewriteUrl += 
+					`	# Redirect everything to www (preserve protocol)
+						RewriteCond %{HTTP_HOST} !^www\. [NC]
+						RewriteCond %{HTTPS} on
+						RewriteRule ^ https://www.%{HTTP_HOST}/%{REQUEST_URI} [L,R=301]
+						RewriteCond %{HTTP_HOST} !^www\. [NC]
+						RewriteCond %{HTTPS} off
+						RewriteRule ^ http://www.%{HTTP_HOST}/%{REQUEST_URI} [L,R=301]
+
+					`;
+					break;
+			
+				case '5':
+					rewriteUrl += 
+					`	# Redirect everything to no-www (preserve protocol)
+						RewriteCond %{HTTP_HOST} ^www\. [NC]
+						RewriteCond %{HTTPS} on
+						RewriteRule ^ https://%{HTTP_HOST}/%{REQUEST_URI} [L,R=301]
+						RewriteCond %{HTTP_HOST} ^www\. [NC]
+						RewriteCond %{HTTPS} off
+						RewriteRule ^ http://%{HTTP_HOST}/%{REQUEST_URI} [L,R=301]
+
+					`;
+					break;
+			
+				case '6':
+					rewriteUrl += 
+					`	# Redirect everything to http + www
+						RewriteCond %{HTTPS} on [OR]
+						RewriteCond %{HTTP_HOST} !^www\. [NC]
+						RewriteRule ^ http://www.%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+
+					`;
+					break;
+			
+				case '7':
+					rewriteUrl += 
+					`	# Redirect everything to http + no-www
+						RewriteCond %{HTTPS} on [OR]
+						RewriteCond %{HTTP_HOST} ^www\. [NC]
+						RewriteRule ^ http://%{HTTP_HOST:4}%{REQUEST_URI} [L,R=301]
+
+					`;
+					break;
+			
+				case '8':
+					rewriteUrl += 
+					`	# Redirect everything to http (no preference for www or no-www)
+						RewriteCond %{HTTPS} on
+						RewriteRule ^ http://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+
+					`;
+					break;
+			
+				default:
+					rewriteUrl += 
+					`	# No redirect or invalid option
+
+					`;
+					break;
+			}
+				
+		}
+
+		//Custom 404 Error Page
+		let error404 = ""
+		if (this.config.redirect404) {
+			error404 = 
+			`	# Custom 404 Error Page
+				ErrorDocument 404 /404.html
+
+			`
+		}
+
+		//Gzip compression
+		let gzip = ""
+		if (this.config.gzip) {
+			gzip = 
+			`	# Enable Gzip compression
+				<IfModule mod_deflate.c>
+					AddOutputFilterByType DEFLATE text/html text/plain text/xml text/css
+					AddOutputFilterByType DEFLATE text/javascript application/javascript application/json
+					AddOutputFilterByType DEFLATE application/x-javascript application/xml application/xml+rss
+					AddOutputFilterByType DEFLATE application/font-woff2 application/font-woff application/font-ttf
+					AddOutputFilterByType DEFLATE image/svg+xml
+				</IfModule>
+			`
+		}
+
+		//Custom directives
+		let custom = ""
+		if (this.config.custom) {
+			custom = 
+			`	# Custom directives
+				${this.config.personal}
+			`
+		}
+
+		content = rewriteUrl + error404 + gzip + custom
+		content = content
+			.replace(/[ \t]+/g, ' ')    // Removes double spaces and tabs, replacing them with a single space
+			.replace(/^[ \t]+/gm, '')   // Removes spaces and tabs at the beginning of each line
+			.trim();                    // Removes spaces at the start and end of the entire string
+
+
+		//create file in root folder
+		this.API.createFile(`[ROOT-FILES]/${output}`, content, this);
+	}
+	
+}
+
+
+module.exports = MyPlugin;
